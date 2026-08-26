3cca20178aeaee08f5918765471e24fba6ef8907
怀天 <2965373537@qq.com>
2026-08-26 21:29:44 +0800

    回退权限申请优化

--- app/src/main/java/fake/screenshot/pages/SettingsPage.kt
+++ app/src/main/java/fake/screenshot/pages/SettingsPage.kt
@@ -5,6 +5,7 @@ import android.app.ActivityManager
 import android.content.ComponentName
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Environment
 import android.provider.Settings
 import android.view.WindowManager
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -110,6 +111,7 @@ fun SettingsCompose(navController: NavController) {
         }
     }
     var daemonConfigDialog by remember { mutableStateOf(false) }
+    var externalStorageRequireDialog by remember { mutableStateOf(false) }
     var fileEncryptionWarnings by remember { mutableStateOf(false) }
     var hideIconWarnings by remember { mutableStateOf(false) }
     var installPackageRequireDialog by remember { mutableStateOf(false) }
@@ -220,11 +222,14 @@ fun SettingsCompose(navController: NavController) {
                         checked = encryptOutputs,
                         onCheckedChange = {
                             scope.launch {
-                                if (it) {
-                                    fileEncryptionWarnings = true
-                                } else {
-                                    ConfigManager.saveData(context, "encrypt_outputs", false)
-                                    DaemonManager.syncConfig()
+                                when {
+                                    !it -> {
+                                        ConfigManager.saveData(context, "encrypt_outputs", false)
+                                        DaemonManager.syncConfig()
+                                    }
+                                    Environment.isExternalStorageManager() -> fileEncryptionWarnings = true
+
+                                    else -> externalStorageRequireDialog = true
                                 }
                             }
                         }
@@ -666,6 +671,36 @@ fun SettingsCompose(navController: NavController) {
                 },
                 dismissButton = {
                     TextButton(onClick = { hideIconWarnings = false }) {
+                        Text(stringResource(R.string.Cancel))
+                    }
+                }
+            )
+        }
+        if (externalStorageRequireDialog) {
+            CenteredAlertDialog(
+                onDismissRequest = { externalStorageRequireDialog = false },
+                title = {
+                    Text(text = stringResource(R.string.tips))
+                },
+                text = {
+                    Text(stringResource(R.string.full_storage_access_required))
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            val intent =
+                                Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
+                                    data = "package:${context.packageName}".toUri()
+                                }
+                            context.startActivity(intent)
+                            externalStorageRequireDialog = false
+                        },
+                    ) {
+                        Text(stringResource(R.string.go_to_settings))
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = { externalStorageRequireDialog = false }) {
                         Text(stringResource(R.string.Cancel))
                     }
                 }
